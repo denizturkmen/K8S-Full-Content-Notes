57e6f5208679a026da566afec9d9b6b33ef36f8c
deniz.turkmen <turkmen_deniz@hotmail.com>
2023-08-19 09:57:34 +0300

    quicksight

---
+++ K8s Set-up/Kubeadm/1- Kubernetes Install/k8s-install.sh
@@ -0,0 +1,10 @@
+!#/bin/bash
+sudo apt install curl apt-transport-https -y
+curl -fsSL  https://packages.cloud.google.com/apt/doc/apt-key.gpg|sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/k8s.gpg
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+
+sudo apt update
+sudo apt install kubelet kubeadm kubectl -y
+sudo apt install -y kubelet=1.25.1-00  kubeadm=1.25.1-00  kubectl=1.25.1-00 
+sudo apt-mark hold kubelet kubeadm kubectl
